7258f7b68c4bb9f2d2ab6aaf6c73dfe9a39277a3
aseongjun99 <aseongjun99@gmail.com>
2024-10-21 20:35:59 +0900

    feat: Add createMail method to send certification page - 사용자가 이메일을 통해 받을 내용 작성

--- src/main/java/com/ssafy/todolist/service/TodoService.java
+++ src/main/java/com/ssafy/todolist/service/TodoService.java
@@ -3,7 +3,8 @@ package com.ssafy.todolist.service;
 import com.ssafy.todolist.domain.Todo;
 import com.ssafy.todolist.domain.TodoDTO;
 import com.ssafy.todolist.repository.TodoRepository;
-
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,12 @@ import java.util.List;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+    private static int number;
+    private final JavaMailSender javaMailSender;
 
-    public TodoService(TodoRepository todoRepository) {
+    public TodoService(TodoRepository todoRepository, JavaMailSender javaMailSender) {
         this.todoRepository = todoRepository;
+        this.javaMailSender = javaMailSender;
     }
 
     @Transactional(readOnly = true)
@@ -38,4 +42,28 @@ public class TodoService {
         Todo todo = todoRepository.getReferenceById(todoId);
         todo.setCompleted(!todo.getCompleted());
     }
+
+    public MimeMessage createMail(String email) {
+        int number = (int) (Math.random() * (90000)) + 100000;
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        try {
+            message.setFrom(email);
+            message.setRecipients(MimeMessage.RecipientType.TO, email);
+            message.setSubject("이메일 인증");
+            StringBuilder body = new StringBuilder();
+            body.append("<h3>요청하신 인증 번호 입니다.</h3>")
+                    .append("<h1>")
+                    .append(number)
+                    .append("</h1>")
+                    .append("<h3>감사합니다.</h3>");
+            message.setText(body.toString(), "UTF-8", "html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+
+
 }
