acd06f11f2d73211758a78ce49b0b2870f385691
aseongjun99 <aseongjun99@gmail.com>
2024-10-24 16:48:20 +0900

    feat: Add AuthenticationDTO

--- src/main/java/com/ssafy/todolist/domain/email/AuthenticationDTO.java
+++ src/main/java/com/ssafy/todolist/domain/email/AuthenticationDTO.java
@@ -4,10 +4,12 @@ public class AuthenticationDTO {
 
     private String authentication;
     private String email;
+    private String authenticationTime;
 
-    public AuthenticationDTO(String authentication, String email) {
+    public AuthenticationDTO(String authentication, String email, String authenticationTime) {
         this.authentication = authentication;
         this.email = email;
+        this.authenticationTime = authenticationTime;
     }
 
     public String getAuthentication() {
@@ -16,6 +18,10 @@ public class AuthenticationDTO {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getAuthenticationTime() {
+        return authenticationTime;
     }
 
 }
