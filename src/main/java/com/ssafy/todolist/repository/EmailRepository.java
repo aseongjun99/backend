7bb98331dc86c6199e8c52d72d4e32cc4c441e46
aseongjun99 <aseongjun99@gmail.com>
2024-10-24 14:57:53 +0900

    feat: Add findByEmail method

--- src/main/java/com/ssafy/todolist/repository/EmailRepository.java
+++ src/main/java/com/ssafy/todolist/repository/EmailRepository.java
@@ -2,9 +2,13 @@ package com.ssafy.todolist.repository;
 
 import com.ssafy.todolist.domain.email.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface EmailRepository extends JpaRepository<Email, Integer> {
+
+    Email findByEmail(String email);
 
 }
