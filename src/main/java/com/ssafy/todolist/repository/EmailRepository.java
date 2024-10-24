97712ae81d6ad43fcfcbe2498c0a198746074095
aseongjun99 <aseongjun99@gmail.com>
2024-10-24 17:50:51 +0900

    refactor: Modify return type EmailDTO to Email

--- src/main/java/com/ssafy/todolist/repository/EmailRepository.java
+++ src/main/java/com/ssafy/todolist/repository/EmailRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.todolist.repository;
 
 import com.ssafy.todolist.domain.email.Email;
+import com.ssafy.todolist.domain.email.EmailDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
