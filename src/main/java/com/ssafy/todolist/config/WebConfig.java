2f57c5444212ffb750b4ea195c37e5053670b5f6
aseongjun99 <aseongjun99@gmail.com>
2024-10-31 14:12:50 +0900

    refactor: Modify allowCredentials to true

--- src/main/java/com/ssafy/todolist/config/WebConfig.java
+++ src/main/java/com/ssafy/todolist/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://ssafysandbox.vercel.app") // 허용할 클라이언트 도메인
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("Content-Type") // 허용할 헤더
-                .allowCredentials(false) // 쿠키 사용 여부 (필요 시 true로 설정)
+                .allowCredentials(true) // 쿠키 사용 여부 (필요 시 true로 설정)
                 .maxAge(3600); // Preflight 요청 캐시 시간 (1시간)
     }
 }
