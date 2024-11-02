cde10614c00ab9058e37777ea470ca5e9f3a9b7a
aseongjun99 <aseongjun99@gmail.com>
2024-11-02 19:57:33 +0900

    feat: Add bean of restTemplate in Config

--- src/main/java/com/ssafy/todolist/config/WebConfig.java
+++ src/main/java/com/ssafy/todolist/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.ssafy.todolist.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Content-Type") // 허용할 헤더
                 .allowCredentials(true) // 쿠키 사용 여부 (필요 시 true로 설정)
                 .maxAge(3600); // Preflight 요청 캐시 시간 (1시간)
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
