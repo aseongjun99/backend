21d13dc663cb5aedda13858bfd44e0c908245343
aseongjun99 <aseongjun99@gmail.com>
2024-11-03 23:57:25 +0900

    feat: Add DTOs for kakao oauth

--- src/main/java/com/ssafy/todolist/oauth/dto/kakao/KakaoTokenResponse.java
+++ src/main/java/com/ssafy/todolist/oauth/dto/kakao/KakaoTokenResponse.java
@@ -1,13 +1,28 @@
-package com.ssafy.todolist.oauth.dto;
+package com.ssafy.todolist.oauth.dto.kakao;
 
-public class AuthSuccessResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class KakaoTokenResponse {
+
+    @JsonProperty("token_type")
     private String tokenType;
+
+    @JsonProperty("id_token")
     private String idToken;
+
+    @JsonProperty("expires_in")
     private Integer expiresIn;
+
+    @JsonProperty("refresh_token_expires_in")
     private Integer refreshTokenExpiresIn;
+
+    @JsonProperty("scope")
     private String scope;
+
+    @JsonProperty("refresh_token")
     private String refreshToken;
+
+    @JsonProperty("access_token")
     private String accessToken;
 
     public String getRefreshToken() {
